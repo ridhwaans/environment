7624a747cfce021ad88fbb7a41f6d951c62db7ef
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2025-01-16 22:37:02 -0800

    [skip ci] mac

--- src/install.sh
+++ src/install.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-  exit 1
-fi
-
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")
 
 echo "SCRIPT_ROOT: $SCRIPT_ROOT"
