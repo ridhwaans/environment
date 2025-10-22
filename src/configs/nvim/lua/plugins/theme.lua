31118ce5df24ea9859ab2c3980bdca72b2308dfc
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2025-10-22 11:37:16 -0700

    nvim theme

--- src/configs/nvim/lua/plugins/theme.lua
+++ src/configs/nvim/lua/plugins/theme.lua
@@ -1,6 +1,8 @@
 return {
 	{
-    "shmerl/neogotham",
+   'https://gitlab.com/shmerl/neogotham.git',
+    lazy = false,
+    priority = 1000,
 	  config = function()
       vim.cmd.colorscheme("neogotham")
     end,
