16efabef458a60cbae794e805a7a9cfe63361893
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-01-19 16:50:32 -0800

    correct rubylsp name

--- src/configs/nvim/lua/plugins/mason-lspconfig.lua
+++ src/configs/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,34 +1,41 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mason-org/mason.nvim",
-    },
-    opts = {
-      ensure_installed = {
-        "bashls",
-        "lua_ls",
-        "clangd",
-        "ts_ls",
-        "pyright",
-        "ruby-lsp",
-      },
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "beautysh", -- formatter
-        "clang-format", -- formatter
-        "stylua", -- formatter
-        "prettier", -- formatter
-        "black", -- formatter
-        "eslint_d", -- linter
-        "ruff", -- linter
-        "rubocop", -- linter
-      },
-    },
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				"bashls",
+				"lua_ls",
+				"clangd",
+				"ts_ls",
+				"pyright",
+				"ruby_lsp",
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"beautysh", -- formatter
+				"clang-format", -- formatter
+				"stylua", -- formatter
+				"prettier", -- formatter
+				"black", -- formatter
+				"eslint_d", -- linter
+				"ruff", -- linter
+				"rubocop", -- linter
+			},
+		},
+	},
 }
