c7d94474f085a05658989cbe9898a6441a9d9366
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-01-19 16:26:50 -0800

    linters, formatters

--- src/configs/nvim/lua/plugins/mason-lspconfig.lua
+++ src/configs/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,23 +1,34 @@
 return {
   {
-    "neovim/nvim-lspconfig"
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  {
     "mason-org/mason-lspconfig.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
-      "mason-org/mason.nvim"
+      "mason-org/mason.nvim",
     },
     opts = {
       ensure_installed = {
         "bashls",
         "lua_ls",
-        "ts_ls"
-      }
-    }
-  }
+        "clangd",
+        "ts_ls",
+        "pyright",
+        "ruby-lsp",
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "beautysh", -- formatter
+        "clang-format", -- formatter
+        "stylua", -- formatter
+        "prettier", -- formatter
+        "black", -- formatter
+        "eslint_d", -- linter
+        "ruff", -- linter
+        "rubocop", -- linter
+      },
+    },
+  },
 }
