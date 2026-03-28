14f4f3889f93885b2744be8a334798c56ea7aaea
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-03-28 11:13:15 -0700

    fix which branch used in installation

--- boot.sh
+++ boot.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 ENVIRONMENT_DIR="${ENVIRONMENT_DIR:-"$HOME/Source/environment"}"
+ENVIRONMENT_BRANCH="${ENVIRONMENT_BRANCH:-main}"
 
 rm -rf "$ENVIRONMENT_DIR"
-git clone -b main https://github.com/ridhwaans/environment.git "$ENVIRONMENT_DIR"
+git clone -b "$ENVIRONMENT_BRANCH" https://github.com/ridhwaans/environment.git "$ENVIRONMENT_DIR"
 
 source $ENVIRONMENT_DIR/install.sh
