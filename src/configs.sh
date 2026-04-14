5035469387cbf5c005fc791326699a40a8a502ea
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-04-14 10:41:44 -0700

    autoindent config.sh, mise install precompiled ruby & tools once

--- src/configs.sh
+++ src/configs.sh
@@ -3,97 +3,97 @@
 set -e
 
 remove_dest() {
-  dest="$1"
-  if [ -L "$dest" ] || [ -f "$dest" ]; then
-    rm -f "$dest"
-  elif [ -d "$dest" ]; then
-    rm -rf "$dest"
-  fi
+    dest="$1"
+    if [ -L "$dest" ] || [ -f "$dest" ]; then
+        rm -f "$dest"
+    elif [ -d "$dest" ]; then
+        rm -rf "$dest"
+    fi
 }
 
 copy_path() {
-  src="$1"
-  dest="$2"
+    src="$1"
+    dest="$2"
 
-  remove_dest "$dest"
-  mkdir -p "$(dirname "$dest")"
+    remove_dest "$dest"
+    mkdir -p "$(dirname "$dest")"
 
-  if [ -d "$src" ]; then
-    cp -r "$src" "$dest"
-  else
-    cp "$src" "$dest"
-  fi
+    if [ -d "$src" ]; then
+        cp -r "$src" "$dest"
+    else
+        cp "$src" "$dest"
+    fi
 }
 
 echo "(1/4) Setting up IDE..."
 if command -v code &>/dev/null; then
-  code --extensions-dir "$XDG_DATA_HOME/code/extensions"
+    code --extensions-dir "$XDG_DATA_HOME/code/extensions"
 
-  while IFS= read -r extension || [ -n "$extension" ]; do
-      code --install-extension "$extension"
-  done < "$CONFIGS_DIR/code/extensions/extensions"
-  code --install-extension PKief.material-icon-theme
+    while IFS= read -r extension || [ -n "$extension" ]; do
+        code --install-extension "$extension"
+    done < "$CONFIGS_DIR/code/extensions/extensions"
+    code --install-extension PKief.material-icon-theme
 fi
 
 if [ $(uname) = Darwin ]; then
-	echo "(mac)"
+    echo "(mac)"
 
-	VSCODE_USER_SETTINGS_DIR=$HOME/Library/Application\ Support/Code/User
-  copy_path "$CONFIGS_DIR/code/user/settings.json" "$VSCODE_USER_SETTINGS_DIR/settings.json"
-
-elif [ $(uname) = Linux ]; then
-	if [ -n "$WSL_DISTRO_NAME" ]; then
-		echo "(wsl)"
-
-    WINDOWS_HOME=$(wslpath $(powershell.exe '$env:UserProfile') | sed -e 's/\r//g')
-    VSCODE_USER_SETTINGS_DIR=$WINDOWS_HOME/AppData/Roaming/Code/User
-
-    # https://github.com/microsoft/vscode/issues/1022
-		# https://github.com/microsoft/vscode/issues/166680
+    VSCODE_USER_SETTINGS_DIR=$HOME/Library/Application\ Support/Code/User
     copy_path "$CONFIGS_DIR/code/user/settings.json" "$VSCODE_USER_SETTINGS_DIR/settings.json"
 
-	elif [ -n "$CODESPACES" ]; then
-		echo "(github codespaces)"
-	else
-		echo "(native linux)"
+elif [ $(uname) = Linux ]; then
+    if [ -n "$WSL_DISTRO_NAME" ]; then
+        echo "(wsl)"
 
-  copy_path "$CONFIGS_DIR/code/user/settings.json" "$XDG_CONFIG_HOME/code/user/settings.json"
+        WINDOWS_HOME=$(wslpath $(powershell.exe '$env:UserProfile') | sed -e 's/\r//g')
+        VSCODE_USER_SETTINGS_DIR=$WINDOWS_HOME/AppData/Roaming/Code/User
 
-	fi
+        # https://github.com/microsoft/vscode/issues/1022
+        # https://github.com/microsoft/vscode/issues/166680
+        copy_path "$CONFIGS_DIR/code/user/settings.json" "$VSCODE_USER_SETTINGS_DIR/settings.json"
+
+    elif [ -n "$CODESPACES" ]; then
+        echo "(github codespaces)"
+    else
+        echo "(native linux)"
+
+        copy_path "$CONFIGS_DIR/code/user/settings.json" "$XDG_CONFIG_HOME/code/user/settings.json"
+
+    fi
 fi
 
 echo "(2/4) Setting up terminal emulator..."
 if [ $(uname) = Darwin ]; then
-	echo "(mac)"
+    echo "(mac)"
 
 elif [ $(uname) = Linux ]; then
-	if [ -n "$WSL_DISTRO_NAME" ]; then
-		echo "(wsl)"
+    if [ -n "$WSL_DISTRO_NAME" ]; then
+        echo "(wsl)"
 
-		WINDOWS_HOME=$(wslpath $(powershell.exe '$env:UserProfile') | sed -e 's/\r//g')
-    TERMINAL_USER_SETTINGS_DIR="$WINDOWS_HOME/AppData/Local/Packages"/Microsoft.WindowsTerminal*/LocalState
+        WINDOWS_HOME=$(wslpath $(powershell.exe '$env:UserProfile') | sed -e 's/\r//g')
+        TERMINAL_USER_SETTINGS_DIR="$WINDOWS_HOME/AppData/Local/Packages"/Microsoft.WindowsTerminal*/LocalState
 
-    copy_path "$CONFIGS_DIR/windowsterminal/settings.json" "$TERMINAL_USER_SETTINGS_DIR/settings.json"
+        copy_path "$CONFIGS_DIR/windowsterminal/settings.json" "$TERMINAL_USER_SETTINGS_DIR/settings.json"
 
-	elif [ -n "$CODESPACES" ]; then
-		echo "(github codespaces)"
-	else
-		echo "(native linux)"
-	fi
+    elif [ -n "$CODESPACES" ]; then
+        echo "(github codespaces)"
+    else
+        echo "(native linux)"
+    fi
 fi
 
 echo "(3/4) Setting up home files & permissions..."
 
 for entry in "$CONFIGS_DIR"/*; do
-  name=$(basename "$entry")
+    name=$(basename "$entry")
 
-  # skip
-  case "$name" in
-    code|windowsterminal) continue ;;
-  esac
+    # skip
+    case "$name" in
+        code|windowsterminal) continue ;;
+    esac
 
-  dest="$XDG_CONFIG_HOME/$name"
-  copy_path "$entry" "$dest"
+    dest="$XDG_CONFIG_HOME/$name"
+    copy_path "$entry" "$dest"
 done
 
 if [ -z "$CODESPACES" ] && [ -d "$HOME/.ssh/" ]; then
@@ -104,12 +104,16 @@ fi
 
 # rehome zsh
 echo 'export ZDOTDIR=$HOME/.config/zsh' > $HOME/.zshenv
-
-# gh auth login -h 'github.com' -p 'ssh' --skip-ssh-key -w
+# ensure zsh history
+mkdir -p $XDG_STATE_HOME/zsh
 
 echo "(4/4) Setting up Source directory..."
 # https://gist.github.com/ridhwaans/08f2fc5e9b3614a3154cef749a43a568
 mkdir -p "$HOME/Source" && curl -sfSL "https://gist.githubusercontent.com/ridhwaans/08f2fc5e9b3614a3154cef749a43a568/raw/scripts.sh" -o "$HOME/Source/scripts.sh" && chmod +x "$HOME/Source/scripts.sh"
+
+export PATH=$XDG_BIN_HOME:$PATH
+mise settings ruby.compile=false
+mise install
 
 # Moving to end because it lapses trailing code
 vim -u "$XDG_CONFIG_HOME/vim/vimrc" +silent! +PlugInstall +PlugClean +qall
